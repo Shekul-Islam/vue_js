2ec7a9a001808e54205fe86a8cf138aa9e7adf24
Shekul-Islam <shekul.bd201@gmail.com>
2024-09-08 11:38:51 +0600

    updated

---
+++ vue-test4/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1>This is Home Page</h1>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<style>
+
+</style>
