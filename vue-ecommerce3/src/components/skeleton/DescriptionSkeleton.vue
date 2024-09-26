1db3d23011cd14290d5c2dddac27e0d993fa16c7
Shekul-Islam <shekul.bd201@gmail.com>
2024-09-26 13:11:38 +0600

    updated

--- vue-ecommerce3/src/components/skeleton/DescriptionSkeleton.vue
+++ vue-ecommerce3/src/components/skeleton/DescriptionSkeleton.vue
@@ -7,18 +7,18 @@
     <!-- Slide 1 -->
     
       <div>
-        <div class="ssc-head-line mb-3"></div>
-        <div class="ssc-line mb-2 w-50"></div>
-        <div class="ssc-line mb-2 w-75"></div>
-        <div class="ssc-line mb-2 w-25"></div>
-        <div class="ssc-line mb-2 w-100"></div>
-        <div class="ssc-line mb-2 w-100"></div>
-        <div class="ssc-head-line w-25 mb-2"></div>
+        <div class="ssc-head-line mb-3 p-3"></div>
+        <div class="ssc-line mb-2 w-50 p-2"></div>
+        <div class="ssc-line mb-2 w-75 p-2"></div>
+        <div class="ssc-line mb-2 w-25 p-2"></div>
+        <div class="ssc-line mb-3 w-100 p-2"></div>
+        <div class="ssc-line mb-3 w-100 p-2"></div>
+        <div class="ssc-head-line w-25 mb-2 p-2"></div>
         <div >
          
           <div style="display: flex; gap: 7px">
-            <div class="ssc-head-line w-25 mb-2 "></div>
-            <div class="ssc-head-line w-25 mb-2"></div>
+            <div class="ssc-head-line w-25 mb-2 p-3"></div>
+            <div class="ssc-head-line w-25 mb-2 p-3"></div>
           </div>
         </div>
       
