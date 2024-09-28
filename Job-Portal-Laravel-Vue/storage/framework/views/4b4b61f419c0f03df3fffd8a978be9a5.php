8139793c10600202618fb0e65d2a0b735ee95507
Shekul-Islam <shekul.bd201@gmail.com>
2024-09-28 19:06:19 +0600

    updated

---
+++ Job-Portal-Laravel-Vue/storage/framework/views/4b4b61f419c0f03df3fffd8a978be9a5.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\Users\Student\Desktop\JobFinder-Job-Portal-Laravel-Vue-Script\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
---
+++ Job-Portal-Laravel-Vue/storage/framework/views/ca72cef00d7fdf6829d8f1fea9f3f418.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH C:\Users\Student\Desktop\JobFinder-Job-Portal-Laravel-Vue-Script\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
